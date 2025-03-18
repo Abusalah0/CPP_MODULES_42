bdb2c94765ef1157d4a56f008a123c15c59ef191
Abusalah0 <109g83@gmail.com>
2025-03-19 01:45:53 +0300

    upload ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abdsalah <abdsalah@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 17:22:54 by abdsalah          #+#    #+#             */
+/*   Updated: 2025/03/16 17:23:01 by abdsalah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char)toupper(argv[i][j]);
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
